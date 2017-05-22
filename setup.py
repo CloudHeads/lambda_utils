25dfcaa46f44bada399cc9b17e4576faefa18003
Christoph Schabert <christoph.schabert@paessler.com>
2017-05-22 15:32:53 +0200

    Solve issue in files

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open('README.rst') as readme_file:
     readme = readme_file.read()
@@ -19,13 +19,13 @@ test_requirements = [
 
 setup(
     name='lambda_utils',
-    version='version='0.2.3'',
+    version='0.2.3',
     description="A collection of AWS Lambda Utils / Decorator for different AWS events e.g. Api Gateway, S3, CloudFormation, CloudWatch ",
     long_description=readme + '\n\n' + history,
     author="Cloudheads",
     author_email='theguys@cloudheads.io',
     url='https://github.com/CloudHeads/lambda_utils',
-    packages=['lambda_utils'],
+    packages=find_packages(exclude=('tests', )),
     package_dir={'lambda_utils': 'lambda_utils'},
     include_package_data=True,
     install_requires=requirements,
