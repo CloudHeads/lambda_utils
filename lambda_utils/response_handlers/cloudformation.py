d9743b6d11efe34390022631442e811595a66689
Christoph Schabert <christoph.schabert@paessler.com>
2017-05-22 10:34:37 +0200

    Add cloud formation to response_handler

---
+++ lambda_utils/response_handlers/cloudformation.py
@@ -0,0 +1,36 @@
+import json
+import logging
+import urllib2
+from lambda_utils.response_handlers import BaseResponseHandler
+
+FAILED = 'FAILED'
+SUCCESS = 'SUCCESS'
+
+
+class Cloudformation(BaseResponseHandler):
+    def on_execution(self, event):
+        self.event = event
+        return BaseResponseHandler.on_execution(self, event)
+
+    def on_exception(self, ex):
+        send_signal(self.event, FAILED, ex.message)
+        BaseResponseHandler.on_exception(self, ex)
+
+
+def send_signal(event, response_status, reason, response_data=None):
+    response_body = json.dumps({
+        'Status': response_status,
+        'Reason': str(reason or 'ReasonCanNotBeNone'),
+        'PhysicalResourceId': event.get('PhysicalResourceId', event['LogicalResourceId']),
+        'StackId': event['StackId'],
+        'RequestId': event['RequestId'],
+        'LogicalResourceId': event['LogicalResourceId'],
+        'Data': response_data or {}
+    })
+    logging.debug(response_body)
+    opener = urllib2.build_opener(urllib2.HTTPHandler)
+    request = urllib2.Request(event['ResponseURL'], data=response_body)
+    request.add_header('Content-Type', '')
+    request.add_header('Content-Length', len(response_body))
+    request.get_method = lambda: 'PUT'
+    opener.open(request)
