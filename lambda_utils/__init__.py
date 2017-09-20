9c9b07fa7f2d36b2e5ccd6c455915478c216fe16
Christoph Schabert <christoph.schabert@paessler.com>
2017-09-20 07:53:38 +0200

    Bump version: 0.2.12 → 0.2.13

--- lambda_utils/__init__.py
+++ lambda_utils/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 __author__ = """CloudHeads"""
 __email__ = 'theguys@cloudheads.io'
-__version__ = '0.2.12'
+__version__ = '0.2.13'
 
 from concurrent.futures import ThreadPoolExecutor
 
